ef5a2896596a14228b73b2606f2b2c2d4a9da6bc
ksgr5566 <ksgr5566@gmail.com>
2020-12-14 20:26:39 +0530

    Update merge-sort.c

--- merge-sort.c
+++ merge-sort.c
@@ -25,27 +25,27 @@ void recursive_sort(int arr[],int start,int end){
 }
 
 void merge(int arr[],int start,int end){
-	int temp[end - start + 1];
-	int i = start, j = mid+1, k = 0;
-	while(i <= mid && j <= end) {
-		if(arr[i] <= arr[j]) {
-			temp[k] = arr[i];
-			k++; i++;
-		}
-		else {
-			temp[k] = arr[j];
-			k++; j++;
-		}
-	}
-	while(i <= mid) {
-		temp[k] = arr[i];
-		k++; i++;
-	}
-	while(j <= end) {
-		temp[k] = arr[j];
-		k++; j++;
-	}
-	for(i = start; i <= end; i++) {
-		arr[i] = temp[i - start];
-	}
+  int temp[end - start + 1];
+  int i = start, j = mid+1, k = 0;
+  while(i <= mid && j <= end) {
+    if(arr[i] <= arr[j]) {
+      temp[k] = arr[i];
+      k++; i++;
+    }
+    else {
+      temp[k] = arr[j];
+      k++; j++;
+    }
+  }
+  while(i <= mid) {
+    temp[k] = arr[i];
+    k++; i++;
+  }
+  while(j <= end) {
+    temp[k] = arr[j];
+    k++; j++;
+  }
+  for(i = start; i <= end; i++) {
+    arr[i] = temp[i - start];
+  }
 }
