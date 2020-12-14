0ef3ac0ff12e3a05a8c3de89e5e3db2f02cf42ba
ksgr5566 <ksgr5566@gmail.com>
2020-12-14 17:57:01 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+n=input()
+n=int(n)
+
+#fibonacci efficient algo 
+l=[1,1]
+for x in range(len(l),n):
+    if n>2:
+        l.append(l[len(l)-1]+l[len(l)-2])
+print(l)
+
+#fibonacci naive recursion
+def fib(n):
+    if(n==0 or n==1):
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+print(fib(n-1))
