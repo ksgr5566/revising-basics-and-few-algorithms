ab3612fdb358c6473261139b9f105a8f74ef3fae
ksgr5566 <ksgr5566@gmail.com>
2020-12-14 22:31:53 +0530

    Create Permutations.cpp

---
+++ CSES-Problem-Set/Introductory-Problems/Permutations.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void fastio(){
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);cout.tie(NULL);
+}
+
+int main(){
+  fastio();
+  int n;cin>>n;
+  if(n==2||n==3) cout<<"NO SOLUTION";
+  else{
+    for(int i=2;i<=n;i+=2) cout<<i<<" ";
+    for(int i=1;i<=n;i+=2) cout<<i<<" ";
+  }
+  return 0;
+}
